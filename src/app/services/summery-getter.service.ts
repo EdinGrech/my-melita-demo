59a03d7cb5d03c4a7ee309010dfc478ea679b87c
Ed <edin.grech@gmail.com>
2023-02-28 09:55:55 +0100

    sorter bug fix, quick sort

--- src/app/services/summery-getter.service.ts
+++ src/app/services/summery-getter.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { OfferResponse } from '../services/interface/OfferResponce';
 import { SubscriptionResponse } from '../services/interface/SubscriptionResponce';
 import { subscription } from '../pages/subsctiption/interfaces/subscription/subscription';
+import { Offer } from '../pages/home/interfaces/offers/offer';
 @Injectable({
   providedIn: 'root',
 })
@@ -32,7 +33,47 @@ export class SummeryGetterService {
       }),
     };
 
-    return this.http.get<OfferResponse>(this.baseurl + 'offers', httpOptions);
+    return this.http
+      .get<OfferResponse>(this.baseurl + 'offers', httpOptions)
+      .pipe(
+        map((res) => {
+          res.offers = quicksort(res.offers);
+          console.log(res.offers);
+          return res;
+          function quicksort(offers: Offer[]): Offer[] {
+            if (offers.length <= 1) {
+              return offers;
+            }
+
+            const pivotIndex = Math.floor(offers.length / 2);
+            const pivotDate = new Date(offers[pivotIndex].contractStartDate);
+
+            const left = [];
+            const right = [];
+
+            for (let i = 0; i < offers.length; i++) {
+              if (i === pivotIndex) {
+                continue;
+              }
+
+              const product = offers[i];
+              const date = new Date(product.contractStartDate);
+
+              if (date < pivotDate) {
+                left.push(product);
+              } else {
+                right.push(product);
+              }
+            }
+
+            return [
+              ...quicksort(left),
+              offers[pivotIndex],
+              ...quicksort(right),
+            ];
+          }
+        })
+      );
   }
 
   subscribe(offerid: number): Observable<SubscriptionResponse> {
@@ -43,49 +84,55 @@ export class SummeryGetterService {
       }),
     };
 
-    let tempRes = this.http.get<SubscriptionResponse>(
-      this.baseurl + 'offers/' + offerid + '/subscriptions',
-      httpOptions
-      );
-    //sort subscriptions by name then by line
-    tempRes.subscribe((res) => {
-      res.subscriptions = mergeSortsubs(res.subscriptions);
-      return res;
-      function mergeSortsubs(subs: subscription[]): subscription[] {
-        if (subs.length <= 1) {
-          return subs;
-        }
-      
-        const middle = Math.floor(subs.length / 2);
-        const left = subs.slice(0, middle);
-        const right = subs.slice(middle);
-      
-        return mergesubs(mergeSortsubs(left), mergeSortsubs(right));
-      }
-      
-      function mergesubs(left: subscription[], right: subscription[]): subscription[] {
-        const result = [];
-        let leftIndex = 0;
-        let rightIndex = 0;
-      
-        while (leftIndex < left.length && rightIndex < right.length) {
-          if (left[leftIndex].name < right[rightIndex].name) {
-            result.push(left[leftIndex]);
-            leftIndex++;
-          } else if (left[leftIndex].name > right[rightIndex].name) {
-            result.push(right[rightIndex]);
-            rightIndex++;
-          } else if (left[leftIndex].line < right[rightIndex].line) {
-            result.push(left[leftIndex]);
-            leftIndex++;
-          } else {
-            result.push(right[rightIndex]);
-            rightIndex++;
+    return this.http
+      .get<SubscriptionResponse>(
+        this.baseurl + 'offers/' + offerid + '/subscriptions',
+        httpOptions
+      )
+      .pipe(
+        map((res) => {
+          res.subscriptions = mergeSortsubs(res.subscriptions);
+          return res;
+          function mergeSortsubs(subs: subscription[]): subscription[] {
+            if (subs.length <= 1) {
+              return subs;
+            }
+
+            const middle = Math.floor(subs.length / 2);
+            const left = subs.slice(0, middle);
+            const right = subs.slice(middle);
+
+            return mergesubs(mergeSortsubs(left), mergeSortsubs(right));
           }
-        }
-        return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-      }
-    });
-    return tempRes;
+
+          function mergesubs(
+            left: subscription[],
+            right: subscription[]
+          ): subscription[] {
+            const result = [];
+            let leftIndex = 0;
+            let rightIndex = 0;
+
+            while (leftIndex < left.length && rightIndex < right.length) {
+              if (left[leftIndex].name < right[rightIndex].name) {
+                result.push(left[leftIndex]);
+                leftIndex++;
+              } else if (left[leftIndex].name > right[rightIndex].name) {
+                result.push(right[rightIndex]);
+                rightIndex++;
+              } else if (left[leftIndex].line < right[rightIndex].line) {
+                result.push(left[leftIndex]);
+                leftIndex++;
+              } else {
+                result.push(right[rightIndex]);
+                rightIndex++;
+              }
+            }
+            return result
+              .concat(left.slice(leftIndex))
+              .concat(right.slice(rightIndex));
+          }
+        })
+      );
   }
 }
